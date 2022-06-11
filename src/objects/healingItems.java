e78ca16aa4d0f2521588f46839f0682c4c14b3b5
Phil <pbachmann@stud.hs-bremen.de>
2022-06-11 17:28:36 +0200

    Damn Daniel

--- src/objects/healingItems.java
+++ src/objects/healingItems.java
@@ -7,12 +7,10 @@ int healing = 30;
 
     public void addHealth(player p){
 
-        if (player.health += healing < 100){
+        if ((player.health + healing) =< 100){
             player.health += healing;
         } else {
             player.health = 100;
         }
     }
-
-
 }
