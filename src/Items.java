2d191695fdc381908fa3c45b4440cc9e18e851a3
Phil <pbachmann@stud.hs-bremen.de>
2022-06-11 17:08:11 +0200

    test

---
+++ src/Items.java
@@ -0,0 +1,17 @@
+package src;
+import src.tools.*;
+
+public abstract class Items {
+
+    Vector2f pos = new Vector2f();
+
+    public Items(){
+        this.pos.setXpos(0.0f);
+
+    }
+}
+
+public class healingItems extends Items{
+
+
+}
