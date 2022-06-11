0bc9bd9b8e17013641b40faba0773c5644c185d3
Phil <pbachmann@stud.hs-bremen.de>
2022-06-11 17:22:07 +0200

    Wassup bruv

--- src/objects/healingItems.java
+++ src/objects/healingItems.java
@@ -3,6 +3,16 @@ import src.tools.*;
 
 
 public class healingItems extends Items{
+int healing = 30;
+
+    public void addHealth(player p){
+
+        if (player.health += healing < 100){
+            player.health += healing;
+        } else {
+            player.health = 100;
+        }
+    }
 
 
 }
