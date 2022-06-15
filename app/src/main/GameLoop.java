a1bfcfcc7e24048fdf5ccb819deaa1420fd0c777
brweber <brweber@stud.hs-bremen.de>
2022-06-15 23:56:05 +0200

    Collision changes, also cleanup

--- app/src/main/GameLoop.java
+++ app/src/main/GameLoop.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import objects.player;
 import objects.worldObjects;
 import tools.Collider;
@@ -10,7 +12,8 @@ public class GameLoop implements Runnable{
     private float updateRate = (1.0f/60.0f) * 1000000000.0f;
     private int i = 0;
 
-    // public player P = new player();
+    private player P = new player();
+    private ArrayList<worldObjects> sObjects;
     
     @Override
     public void run() {
@@ -18,7 +21,8 @@ public class GameLoop implements Runnable{
       // P.setHeight(20.0f);
       // P.setWidth(40.0f);
       // P.setPos(new Vector2f(-10f,-10f));
-      
+
+
       while (running == true) {
           float currentTime = System.nanoTime();
           float dTime = currentTime - lastUpdate;
@@ -39,20 +43,29 @@ public class GameLoop implements Runnable{
         i++;
       }
       updateCollision();
+
+
+      updatePositions();
     }
 
-    public void updateMovement() {
+    private void updateMovement() {
         
     }
 
-    public void updateCollision() {                                              //currently just Testcode inside
-
+    private void updateCollision() {                                              //currently just Testcode inside
+      
+      Collider.getCollisisions(P, sObjects);
 
       // worldObjects R = new worldObjects();
       // if(Collider.isColliding(P, R) == true){
       //   System.out.println("yes");
       //   P.setPos(new Vector2f(P.getPos().getXpos()+0.01667f, -10f));
       // } 
+
+    }
+
+    private void updatePositions(){
+      P.updatePosition();
 
     }
 
