309b944877f589f15ab8e4b29d32e6caa4d206b2
Phil <pbachmann@stud.hs-bremen.de>
2022-06-22 14:44:12 +0200

    Gui

---
+++ app/src/main/GUI/GamePanel.java
@@ -0,0 +1,29 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel {
+
+    // 
+    public int screenWidth = 1920;
+
+    public int screenHeight = 1080;
+
+    public GamePanel() {
+
+        // size of the GamePanel
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        
+        // sets background color of the GamePanel to black
+        this.setBackground(Color.black);
+    }
+
+    
+
+
+    
+}
--- app/src/main/Main.java
+++ app/src/main/Main.java
@@ -1,8 +1,38 @@
-public class Main {
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import GUI.GamePanel;
+
+public class Main extends JFrame{
 
     public static void main(String[] args) {
-        GameLoop gameLoop = new GameLoop();
-        Thread t1 = new Thread(gameLoop);
-        t1.start();
+
+        JFrame window = new JFrame();
+
+        // allows the user to exit the window
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // background color
+        window.setBackground(Color.black);
+
+        // window size is constant
+        window.setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+
+        window.add(gamePanel);
+
+        // window is created in the center of the screen
+        window.setLocationRelativeTo(null);
+
+        // The size of the Frame equals the GamePanel
+        window.pack();
+
+        window.setVisible(true);
+
+        // GameLoop gameLoop = new GameLoop();
+        // Thread t1 = new Thread(gameLoop);
+        // t1.start();
     }
 }
