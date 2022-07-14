8591362a9a63944f74fc16e18b79cd12e720cfc3
D. Rafii Vardiny <drafii-vardiny@stud.hs-bremen.de>
2022-07-14 23:31:36 +0200

    neue level

--- app/src/main/test/LoadLevel.java
+++ app/src/main/test/LoadLevel.java
@@ -10,32 +10,49 @@ public class LoadLevel {
     public LoadLevel(){
 
         this.addStage();
-        this.addObject(0, 20, 300, new Vector2f(200, 375), "box", 0);
-        this.addObject(0, 20, 200, new Vector2f(600, 425), "box", 0);
+
+        this.addObject(0, 20, 300, new Vector2f(900, 450), "box", 0);
+        this.addObject(0, 300, 200, new Vector2f(600, 100), "box", 0);
         this.addObject(0, 10000, 50, new Vector2f(0, 0), "box", 0);
 
         //World Loadboarders (copy paste)
         this.addObject(0, 10000, 50, new Vector2f(-60, 0), "leftTrigger", 0);
         this.addObject(0, 10000, 50, new Vector2f(1920, 0), "rightTrigger", 0);
 
+        //---------
 
         this.addStage();
-        this.addObject(1, 10, 500, new Vector2f(100, 100), "box", 0);
-        this.addObject(1, 20, 300, new Vector2f(600, 425),"box", 0);
-        this.addObject(1, 50, 50, new Vector2f(400,400),"trap", 100);
 
+        this.addObject(1, 20, 300, new Vector2f(600, 400),"box", 0);
+        this.addObject(1, 50, 50, new Vector2f(400,300),"trap", 20);
+        this.addObject(1, 50, 50, new Vector2f(700,420),"trap", 20);
+
+        
         //World Loadboarders (copy paste)
         this.addObject(1, 10000, 50, new Vector2f(-60, 0), "leftTrigger", 0);
         this.addObject(1, 10000, 50, new Vector2f(1920, 0), "rightTrigger", 0);
 
+        //---------
 
         this.addStage();
-        this.addObject(2, 500, 300, new Vector2f(600, 350), "box", 0);
-        this.addObject(2, 500, 300, new Vector2f(1100, 350), "box", 0);
-        this.addObject(2, 50, 200, new Vector2f(1300, 350), "box", 0);
 
+        this.addObject(2, 70, 50, new Vector2f(200, 300), "box", 0);
+        this.addObject(2, 70, 50, new Vector2f(1000, 300), "box", 0);
+        this.addObject(2, 50, 750, new Vector2f(250,300),"trap", 20);
+
+
+        //World Loadboarders (copy paste)
+        this.addObject(2, 10000, 50, new Vector2f(-60, 0), "leftTrigger", 0);
+        this.addObject(2, 10000, 50, new Vector2f(1920, 0), "rightTrigger", 0);
+
+        //---------
 
         this.addStage();  
+
+
+        //World Loadboarders (copy paste)
+        this.addObject(2, 10000, 50, new Vector2f(-60, 0), "leftTrigger", 0);
+        this.addObject(2, 10000, 50, new Vector2f(1920, 0), "rightTrigger", 0);
         
     };
     public void addStage(){
