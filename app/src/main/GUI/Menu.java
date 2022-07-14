afb359c0ebff709ccd030a87badc1e5489b4dbf6
Phil <pbachmann@stud.hs-bremen.de>
2022-07-14 13:42:01 +0200

    Little Menu Update

--- app/src/main/GUI/Menu.java
+++ app/src/main/GUI/Menu.java
@@ -5,7 +5,7 @@ import GUI.GamePanel;
 import java.awt.Graphics2D;
 
 import java.awt.Color;
-
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,10 +19,10 @@ public class Menu extends JFrame{
     private int BHeight = 100;
     private int BWidth = 250;
 
+    private int centerScreenX = 1525/2;
+    private int centerScreenY = 830/2;
 
-
-    private int centerX = 1525/2 - BWidth/2;
-    private int centerY = 830/2 - BHeight/2;
+    final String menu = "MENU";
 
     JLabel label;
 
@@ -61,16 +61,26 @@ public class Menu extends JFrame{
     }
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.green);
+       titleString(g2);
 
-        g2.fillRect(centerX, centerY, BWidth, BHeight);
+        // g2.fillRect(centerX, centerY, BWidth, BHeight);
+    }
+    public int getCenterXString(String text, Graphics2D g2) {
 
-        
+        int centerX = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth()/(2);
 
-        
+        return centerX;
+    }
+    public void titleString(Graphics2D g2) {
+
+        g2.setFont(new Font("Serif", Font.BOLD, 64));
+
+        int reallyCenteredX = centerScreenX - getCenterXString(menu, g2);
+
+        g2.setColor(Color.white);
+
+        g2.drawString(menu, reallyCenteredX, 150);
 
 
     }
-    
-    
 }
