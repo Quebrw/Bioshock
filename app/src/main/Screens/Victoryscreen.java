6a2f03ed288aa298a4486567c8723f7e56b63645
Phiiiiiiil <pbachmann@stud.hs-bremen.de>
2022-07-16 21:47:26 +0200

    victory

---
+++ app/src/main/Screens/Victoryscreen.java
@@ -0,0 +1,39 @@
+package Screens;
+
+//----Graphical Stuff----//
+
+import java.awt.Color;
+
+import java.awt.Graphics2D;
+
+public class Victoryscreen extends Screen{
+
+    // Image size
+    private int centerX = 1920/2;
+    private int centerY = 1080/2;
+    private int imageWidth = 1920;
+    private int imageHeight = 1080;
+    private int offsetX = imageWidth/2;
+    private int offsetY = imageHeight/2;
+
+    private String victory = "Victory";
+
+    public void draw(Graphics2D g2) {
+
+        // transfersImage
+        image = dO2.returnVictory();
+
+        // Draws Background
+        g2.drawImage(image, centerX - offsetX , centerY - offsetY, imageWidth, imageHeight, null);
+
+        // Draws title
+        drawString(g2, 80, victory, 150, Color.black);
+
+        // Draws text
+        drawString(g2, 40, "To start press Enter", 900, Color.black);
+
+    }
+    
+    
+    
+}
